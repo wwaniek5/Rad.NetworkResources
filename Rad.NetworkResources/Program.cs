60958f45d149dd9b1b3c8966b5271056134b441f
Wojtek <Wojtek@DESKTOP-VCD360F>
2016-11-20 19:45:00 +0100

    works

--- Rad.NetworkResources/Program.cs
+++ Rad.NetworkResources/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.DirectoryServices;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,36 @@ namespace Rad.NetworkResources
     {
         static void Main(string[] args)
         {
+            List<string> networkResources = GetNetworkResources();
+            WriteToFile(networkResources);
+
+        }
+
+        private static void WriteToFile(List<string> networkResources)
+        {
+            const string fileName = "NetworkResources.txt";
+            var file=File.Create(fileName);
+            file.Close();
+             File.AppendAllLines(fileName, networkResources);
+        }
+
+        private static List<string> GetNetworkResources()
+        {
+            List<string> networkResources=new List<string>();
+
+            DirectoryEntry root = new DirectoryEntry("WinNT:");
+            foreach (DirectoryEntry computers in root.Children)
+            {
+                foreach (DirectoryEntry computer in computers.Children)
+                {
+                    if (computer.Name != "Schema")
+                    {
+                        networkResources.Add(computer.Name);
+                    }
+                }
+            }
+
+            return networkResources;
         }
     }
 }
